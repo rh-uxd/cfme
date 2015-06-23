d5a49a0b0c0266457989005ce77665bddbc34478
Eleni Rundle <erundle@Elenis-MacBook-Pro.local>
2015-06-23 16:54:21 -0400

    Updating to proper header

--- angular/src/modules/app/controllers/app-controller.js
+++ angular/src/modules/app/controllers/app-controller.js
@@ -12,38 +12,95 @@ angular.module( 'cfme.appModule' )
          vm.username = "Administrator";
 
          vm.navigationItems = [
-				{
-					"title": "Cloud Intelligence",
-					"href": "#/ci"
-				},
-				{
-					"title": "Services",
-					"href": "#/services"
-				},
-				{
-					"title": "Clouds",
-					"href": "#/clouds"
-				},
-				{
-					"title": "Intrastructure",
-					"href": "#/intrastructure"
-				},
-				{
-					"title": "Control",
-					"href": "#/control"
-				},
-				{
-					"title": "Automate",
-					"href": "#/automate"
-				},
-				{
-					"title": "Optimize",
-					"href": "#/optimize"
-				},
-				{
-					"title": "Configure",
-					"href": "#/configure"
-				}
-			];
+			{
+				"title": "Cloud Intelligence",
+				"href": "#/ci"
+			},
+			{
+				"title": "Services",
+				"href": "#/services"
+			},
+			{
+				"title": "Clouds",
+				"href": "#/clouds"
+			},
+			{
+				"title": "Intrastructure",
+				"href": "#/intrastructure"
+			},
+			{
+				"title": "Containers",
+				"href": "#/containers",
+				"class": "active",
+				"children": [
+					{
+						"title": "Nodes",
+						"href": "#/nodes"
+					},
+					{
+						"title": "Container Groups",
+						"href": "#/containergroups",
+						"class": "active"
+					},
+					{
+						"title": "Routes",
+						"href": "#/routes"
+					},
+					{
+						"title": "Replicators",
+						"href": "#/replicators"
+					},
+					{
+						"title": "Images",
+						"href": "#/images"
+					},
+					{
+						"title": "Image Registries",
+						"href": "#/imagereg"
+					},
+					{
+						"title": "Services",
+						"href": "#/serives"
+					},
+					{
+						"title": "Containers",
+						"href": "#/containers"
+					},
+					{
+						"title": "Topology",
+						"href": "#/topology"
+					}
+				]
+			},
+			{
+				"title": "Control",
+				"href": "#/control"
+			},
+			{
+				"title": "Automate",
+				"href": "#/automate"
+			},
+			{
+				"title": "Optimize",
+				"href": "#/optimize"
+			},
+			{
+				"title": "Configure",
+				"href": "#/configure"
+			}
+		];
+
+		vm.notifications = [
+			{
+				"text": "Modified Datasources ExampleDS"
+			},
+			{
+				"text": "Error: System Failure"
+			}
+		];
+
+		vm.clearNotifications = function() {
+			vm.notifications = [];
+		}
 
      }] );
