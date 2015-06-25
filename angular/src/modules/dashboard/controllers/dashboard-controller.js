a1fa3c66c7983d511ca9b922573d70d81ad49968
David Taylor <dtaylor@redhat.com>
2015-06-24 22:13:08 -0400

    Made status Icons & Count dynamic in object-status tile

--- angular/src/modules/dashboard/controllers/dashboard-controller.js
+++ angular/src/modules/dashboard/controllers/dashboard-controller.js
@@ -10,11 +10,11 @@ angular.module( 'cfme.dashboardModule' )
         var vm = this;
         
         vm.status_widgets = [ 
-                { typeName: 'Nodes', typeIconClass: 'pficon-container-node', count: 52, error: { errorIconClass: 'pficon-error-circle-o', errorCount: 3}},
+                { typeName: 'Nodes', typeIconClass: 'pficon-container-node', count: 52, status: [{statusIconClass: 'pficon-error-circle-o', statusCount: 3}]},
                 { typeName: 'Containers', typeIconClass: 'fa-cube', count: 300 },
                 { typeName: 'Registries', typeIconClass: 'pficon-registry', count: 4 },
                 { typeName: 'Projects', typeIconClass: 'pficon-project', count: 510 },
-                { typeName: 'Container Groups', typeIconClass: 'fa-cubes', count: 1200, error: { errorIconClass: 'pficon-error-circle-o', errorCount: 3}},
+                { typeName: 'Container Groups', typeIconClass: 'fa-cubes', count: 1200, status: [{statusIconClass: 'pficon-error-circle-o', statusCount: 3}]},
                 { typeName: 'Services', typeIconClass: 'pficon-service', count: 2500 },
                 { typeName: 'Images', typeIconClass: 'pficon-image', count: 2500 },
                 { typeName: 'Routes', typeIconClass: 'pficon-route', count: 300 }
