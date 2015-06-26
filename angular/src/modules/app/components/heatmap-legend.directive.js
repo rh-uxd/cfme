5a4d8e4940cfcab21f6bac6400430f13f8502dc4
Jeffrey Phillips <jephilli@redhat.com>
2015-06-26 17:01:21 -0400

    Add heat maps - no tooltips, no resize, and poor legend layout

---
+++ angular/src/modules/app/components/heatmap-legend.directive.js
@@ -0,0 +1,24 @@
+'use strict';
+angular.module('patternfly.pf-components').directive('pfHeatMapLegend', ['ChartsMixin', '$timeout', function(chartsMixin, $timeout) {
+    return {
+        restrict: 'A',
+        scope: {
+            legend: '='
+        },
+        replace: true,
+        templateUrl: 'modules/app/components/heatmap-legend.directive.html',
+        controller: function ($scope, $rootScope) {
+            var items = [];
+            var legendColors = chartsMixin.getDefaultHeatmapColorPattern();
+
+            if ($scope.legend)
+            {
+                for (var i = $scope.legend.length - 1; i >= 0; i--)
+                {
+                    items.push({text: $scope.legend[i], color: legendColors[i]});
+                }
+            }
+            $scope.legend_items = items;
+        }
+    }
+}]);
--- angular/src/modules/containers/dashboard/controllers/dashboard-controller.js
+++ angular/src/modules/containers/dashboard/controllers/dashboard-controller.js
@@ -343,5 +343,97 @@ angular.module('cfme.containers.dashboardModule').controller('containers.dashboa
             ],
             dates: []
         };
+
+        var randomInt = function (min, max) {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        };
+
+        var nodeObjectId = 0;
+
+        var getUsedValue = function(total) {
+            var used;
+
+            var bucketPercent = randomInt(0, 100);
+            if (bucketPercent < 65) {
+                used = Math.floor(randomInt(20, (total * 0.70)));
+            }
+            else if (bucketPercent < 85) {
+                used = Math.floor(randomInt((total * 0.70), (total * 0.80)));
+            }
+            else if (bucketPercent < 95) {
+                used = Math.floor(randomInt((total * 0.80), (total * 0.90)));
+            }
+            else {
+                used = Math.floor(randomInt((total * 0.90), total));
+            }
+
+            return used;
+        };
+
+        var randomizeData = function (openShiftCount, kubernetesCount) {
+            var newData = [];
+
+            for (var i = 0; i < openShiftCount; i++) {
+                var total = 100;
+                var used = getUsedValue(total);
+                var available = total - used;
+                var value = used / total;
+                var valuePercent = Math.floor(value * 100);
+
+                newData.push({
+                    id: nodeObjectId++,
+                    value: value,
+                    tooltip: [
+                        'Node ' + k + ' : My OpenShift Provider',
+                        '' + valuePercent + '% : ' + used + ' Used of ' + total + ' Total',
+                        '' + available + ' Available'
+                    ].join('<br/>')
+                });
+            }
+
+            for (var k = 0; k < kubernetesCount; k++) {
+                var total = 100;
+                var used = getUsedValue(total);
+                var available = total - used;
+                var value = used / total;
+                var valuePercent = Math.floor(value * 100);
+
+                newData.push({
+                    id: nodeObjectId++,
+                    value: value,
+                    tooltip: [
+                        'Node ' + k + ' : My Kubernetes Provider',
+                        '' + valuePercent + '% : ' + used + ' Used of ' + total + ' Total',
+                        '' + available + ' Available'
+                    ].join('<br/>')
+                });
+            }
+
+            newData.sort(function (a, b) {
+                return b.value - a.value;
+            });
+
+            return newData;
+        };
+
+        // FAKE NODE USAGE FOR NOW
+        var openShiftCount = 25;
+        var kubernetesCount = 27;
+        $scope.nodeCpuUsage = {
+            data: randomizeData(openShiftCount, kubernetesCount)
+        };
+        $scope.nodeMemoryUsage = {
+            data: randomizeData(openShiftCount, kubernetesCount)
+        };
+        $scope.nodeStorageUsage = {
+            data: randomizeData(openShiftCount, kubernetesCount)
+        };
+        $scope.nodeNetworkUsage = {
+            data: randomizeData(openShiftCount, kubernetesCount)
+        };
+
+        $scope.nodeHeatMapUsageLegendLabels = ['< 70%', '70-80%' ,'80-90%', '> 90%'];
+        $scope.nodeHeatMapNetworkLegendLabels = ['Very High','High','Low', 'Very Low'];
+
     }
 ]);
