5860c46a6d6c3039fa1f6557d199adf4a4539e4f
David Taylor <dtaylor@redhat.com>
2015-07-13 10:43:32 -0400

    Refactoring of 'src/modules/containers', removed controller & views sub-dirs

--- angular/src/modules/containers/containers-module.js
+++ angular/src/modules/containers/containers-module.js
@@ -3,22 +3,22 @@ angular.module( 'cfme.containersModule', ['cfme.containers.projectsModule', 'cfm
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider
         	.when('/containers/dashboard', {
-            	templateUrl: 'modules/containers/dashboard/views/dashboard.html',
+                templateUrl: 'modules/containers/dashboard/dashboard.html',
                 controller: 'containers.dashboardController',
                 controllerAs: 'vm'
             })
             .when('/containers/projects', {
-             	templateUrl: 'modules/containers/projects/views/projects.html',
+                templateUrl: 'modules/containers/projects/projects.html',
                 controller: 'containers.projectsController',
                 controllerAs: 'vm'
              })
             .when('/containers/providers', {
-           		templateUrl: 'modules/containers/providers/views/providers.html',
+                templateUrl: 'modules/containers/providers/providers.html',
                 controller: 'containers.providersController',
                 controllerAs: 'vm'
              })
              .when('/containers/providers/:id', {
-           		templateUrl: 'modules/containers/providers/views/providers.html',
+                templateUrl: 'modules/containers/providers/providers.html',
                 controller: 'containers.providersController',
                 controllerAs: 'vm'
              })
