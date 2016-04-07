725f8e57737c1339b1c0bc8648f95c105cf97c1e
Jeffrey Phillips <jephilli@redhat.com>
2016-04-07 07:52:20 -0400

    Provider deployment wizard page updates
    Includes page for specifying a list of masters/nodes.

--- angular/src/modules/containers/providers/deploy-provider/deploy-provider-details-general-controller.js
+++ angular/src/modules/containers/providers/deploy-provider/deploy-provider-details-general-controller.js
@@ -1,48 +1,59 @@
 angular.module('miq.containers.providersModule').controller('containers.deployProviderDetailsGeneralController',
-  ['$rootScope', '$scope',
-  function($rootScope, $scope) {
+  ['$rootScope', '$scope', '$document', '$timeout',
+  function($rootScope, $scope, $document, $timeout) {
     'use strict';
 
-    $scope.existingProviders = [
-      {
-        id: 1,
-        name: 'Existing Provider 1'
-      },
-      {
-        id: 2,
-        name: 'Existing Provider 2'
-      },
-      {
-        id: 3,
-        name: 'Existing Provider 3'
-      },
-      {
-        id: 4,
-        name: 'Existing Provider 4'
-      }
-    ];
-    $scope.data.existingProviderId = $scope.existingProviders[0].id;
-    $scope.newVmProviders = [
-      {
-        id: 1,
-        name: 'Existing Provider 1'
-      },
-      {
-        id: 2,
-        name: 'Existing Provider 2'
-      },
-      {
-        id: 3,
-        name: 'Existing Provider 3'
-      },
-      {
-        id: 4,
-        name: 'Existing Provider 4'
-      }
-    ];
-    $scope.data.newVmProviderId = $scope.existingProviders[0].id;
+    var firstShow = true;
+    $scope.onShow = function () {
+      if (firstShow) {
+        $scope.existingProviders = [
+          {
+            id: 1,
+            name: 'Existing Provider 1'
+          },
+          {
+            id: 2,
+            name: 'Existing Provider 2'
+          },
+          {
+            id: 3,
+            name: 'Existing Provider 3'
+          },
+          {
+            id: 4,
+            name: 'Existing Provider 4'
+          }
+        ];
+        $scope.data.existingProviderId = $scope.existingProviders[0].id;
+        $scope.newVmProviders = [
+          {
+            id: 1,
+            name: 'Existing Provider 1'
+          },
+          {
+            id: 2,
+            name: 'Existing Provider 2'
+          },
+          {
+            id: 3,
+            name: 'Existing Provider 3'
+          },
+          {
+            id: 4,
+            name: 'Existing Provider 4'
+          }
+        ];
+        $scope.data.newVmProviderId = $scope.existingProviders[0].id;
 
-    $scope.deploymentDetailsGeneralComplete = false;
+        $scope.deploymentDetailsGeneralComplete = false;
+        firstShow = false;
+
+        $timeout(function() {
+          var queryResult = $document[0].getElementById('new-provider-name');
+          queryResult.focus();
+        }, 200);
+      }
+    };
     $scope.updateProviderName = function() {
       $scope.deploymentDetailsGeneralComplete = angular.isDefined($scope.data.providerName) && $scope.data.providerName.length > 0;
     };
