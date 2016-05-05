307a2774c6da4d965384095377cfc32e0b5a5e54
dkorn <dkorn@redhat.com>
2016-05-05 14:30:31 +0300

    Changes in settings substep

--- angular/src/modules/containers/providers/deploy-provider/deploy-provider-config-settings-controller.js
+++ angular/src/modules/containers/providers/deploy-provider/deploy-provider-config-settings-controller.js
@@ -7,12 +7,14 @@ angular.module('miq.containers.providersModule').controller('containers.deployPr
     var firstShow = true;
     $scope.onShow = function () {
       if (firstShow) {
+        $scope.data.configureIntegratedNFS = false;
         $scope.data.configureRouter = false;
         $scope.data.configureRegistry = false;
-        $scope.data.useDefaultRegistry = false;
-        $scope.data.useS3Registry = false;
-        $scope.data.useSwift = false;
         $scope.data.configureMetrics = false;
+        $scope.data.registryServer = '';
+        $scope.data.registryPath = '';
+        $scope.data.metricsServer = '';
+        $scope.data.metricsPath = '';
         firstShow = false;
       }
       $scope.validateForm();
@@ -21,6 +23,20 @@ angular.module('miq.containers.providersModule').controller('containers.deployPr
         var queryResult = $document[0].getElementById('rhn-user-name');
         queryResult.focus();
       }, 200);
+
+      //$timeout(function() {
+      //  if ($scope.data.configureRegistry) {
+      //    var queryResult = $document[0].getElementById('registry-server');
+      //    queryResult.focus();
+      //  }
+      //}, 200);
+
+    //  $timeout(function() {
+    //    if ($scope.data.configureRegistry) {
+    //      var queryResult = $document[0].getElementById('metrics-server');
+    //      queryResult.focus();
+    //    }
+    //  }, 200);
     };
 
     var validString = function(value) {
@@ -28,11 +44,23 @@ angular.module('miq.containers.providersModule').controller('containers.deployPr
     };
 
     $scope.validateForm = function() {
-      $scope.configStorageComplete =
+      var configStorageComplete =
         $scope.data.storageType !== 'nfs' ||
         (validString($scope.data.nfsStorageUsername) &&
          validString($scope.data.nfsStoragePassword) &&
          validString($scope.data.nfsStorageUrl));
+
+      var configRegistryComplete =
+        !$scope.data.configureRegistry ||
+        (validString($scope.data.registryServer) &&
+        validString($scope.data.registryPath));
+
+      var configMetricsComplete =
+        !$scope.data.configureMetrics ||
+        (validString($scope.data.metricsServer) &&
+        validString($scope.data.metricsPath));
+
+      $scope.configSettingsComplete = configStorageComplete && configRegistryComplete && configMetricsComplete;
     };
   }
 ]);
